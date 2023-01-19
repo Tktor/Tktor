6b8d8e7e1eccb843874083c2b7a5e5278b1252d2
김현준 <supply900219@gmail.com>
2023-01-19 11:50:59 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,10 @@
+function solution(num) {
+//     if(num % 2 !== 0) {
+//         return "Odd"
+//     } else {
+//         return "Even"
+//     }
+//     return answer;
+// }
+    return num % 2 === 0 ? "Even" : "Odd" // 삼항연산자
+}
