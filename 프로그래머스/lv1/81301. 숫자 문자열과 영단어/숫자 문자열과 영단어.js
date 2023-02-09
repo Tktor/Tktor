6a4c5b636fa1434a802ef247b8caa6c45dc9a248
김현준 <supply900219@gmail.com>
2023-02-09 10:24:36 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,10 +1,22 @@
+// replace
+// 1. 문자열에서 사용 가능
+// 2. 첫번째 인자로 넘겨준 문자열을 두번쨰 인자로 넘겨준 문자열로 변겅
+
+// replaceAll
+// 1. 주어진 문자열에서 조건에 부합하는 모든 문자열을 치환
+
 const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 function solution(s) {
-  for(let i = 0; i < numbers.length; i++) {
-    while(s.includes(numbers[i])) {
-      s = s.replace(numbers[i], i)
-    }
-  }
+  numbers.forEach((num, i) => {
+    s = s.split(num).join(i)
+  })
+  // for(let i = 0; i < numbers.length; i++) {
+    // while(s.includes(numbers[i])) {
+    //   s = s.replace(numbers[i], i)
+    // }
+    
+    // s = s.replaceAll(numbers[i], i)
+  // }
   return Number(s)
 }
