6f100fb1b3202968933f4e3aa74e17d41690643c
김현준 <supply900219@gmail.com>
2023-02-23 09:35:39 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,11 @@
 function solution(s) {
+  // 문자열 공백 기준으로 배열로 만들어서 오름차순 정렬  
   const result = s.split(" ").sort((a, b) => a - b)
+  // s의 최솟값
   const min = result[0]
+  // s의 최댓값
   const max = result[result.length - 1]
   
+  // 공백과 함께 최솟값, 최댓값 리턴
   return answer = min + " " + max
 }
